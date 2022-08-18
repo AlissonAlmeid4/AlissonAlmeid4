ba2519e11703a41b5f4ca2a4d7b7eba20ec34263
Alisson <alisson@rosset.com.br>
2022-08-18 13:30:49 -0300

    tenta né

---
+++ src/pages/AboutPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <MensagemNossaHistoria/>
+
+  <FooTer/>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import MensagemNossaHistoria from 'src/layouts/mensagemNossaHistoria.vue';
+import FooTer from 'src/layouts/FooTer.vue';
+
+export default defineComponent({
+    name: "AboutPage",
+    components: { MensagemNossaHistoria, FooTer }
+})
+</script>
---
+++ src/pages/PresentPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <SujestaoPresentes/>
+  <EscolherPresente/>
+  <ListaPresentes/>
+
+  <FooTer/>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import SujestaoPresentes from '../layouts/SujestaoPresentes';
+import ListaPresentes from '../layouts/ListaPresentes';
+import EscolherPresente from 'src/layouts/escolherPresente.vue';
+import FooTer from 'src/layouts/FooTer.vue';
+
+export default defineComponent({
+    name: "PresentPage",
+    components: { SujestaoPresentes, ListaPresentes, EscolherPresente, FooTer }
+})
+</script>
